98fff5c8f608e1f031849f2a2cdd6d945bb1be43
hudson <hfinn@autodist.com>
2024-02-08 19:18:21 -0500

    server

--- src/main.c
+++ src/main.c
@@ -1,1 +1,12 @@
-int main(int argc, char *argv[]) { return 0; }
+#include <sys/socket.h>
+
+int main(int argc, char *argv[]) {
+  int socket_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+
+  if (socket_fd == -1) {
+    // We have an error and should exit
+    return -1;
+  }
+
+  return 0;
+}

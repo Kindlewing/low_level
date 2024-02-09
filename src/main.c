313118c9ba3b1ac566b15e7018d2ed6b50f7e92c
hudson <hfinn@autodist.com>
2024-02-08 20:38:00 -0500

    Include socketaddr header file

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
+#include <netinet/in.h>
 #include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
-  int socket_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+	struct sockaddr_in *address;
 
-  if (socket_fd == -1) {
-    // We have an error and should exit
-    return -1;
-  }
+	int socket_fd = socket(AF_LOCAL, SOCK_STREAM, 0);
 
-  return 0;
+	if (socket_fd == -1) {
+		return -1;
+	}
+
+	return 0;
 }
